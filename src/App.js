28b4085c84e3d5c9e164a11f0174df492692e51f
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 18:12:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
     const [data, setData] = useState(null);
+    const id = '528560dc-0507-4db9-94f9-f1afa80d0e07'        
 
+    const api = `https://fe-test.marketing4storage.com/cart/reserve/${id}`;
+
+   
+    
      useEffect(() => {
-         try{
-    fetch('https://fe-test.marketing4storage.com/cart/reserve/528560dc-0507-4db9-94f9-f1afa80d0e07')
-      .then((res) => res.json())
-      .then((resJson) => {
-        const dataCollected = resJson
-        console.log(dataCollected)
-        setData(dataCollected)
-    })
-         } catch (error) {
-                console.log("error", error);
+          console.log("start call api")
+    fetch(api)
+        .then((response) => {
+            if(response.status === 200){
+                console.log("SUCCESSS")
+                return response.json();     
+            }else if(response.status === 408){
+                console.log("SOMETHING WENT WRONG")
+                this.setState({ requestFailed: true })
             }
+        })
+        .then((data) => {
+            this.setState({ isLoading: false, downlines: data.response })
+            console.log("DATA STORED")
+        })
+        .catch((error) => {
+            this.setState({ requestFailed: true })
+        })
   }, [])
 
     return (
