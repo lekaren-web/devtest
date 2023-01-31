738a66490ba4f88855b11142f6f2a50ebc66c9cc
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 18:15:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
     const [data, setData] = useState(null);
+    const [requestFailed, setrequestFailed] = useState(null);
     const id = '528560dc-0507-4db9-94f9-f1afa80d0e07'        
 
     const api = `https://fe-test.marketing4storage.com/cart/reserve/${id}`;
@@ -19,15 +20,15 @@ const App = () => {
                 return response.json();     
             }else if(response.status === 408){
                 console.log("SOMETHING WENT WRONG")
-                this.setState({ requestFailed: true })
+                setrequestFailed(true)
             }
         })
-        .then((data) => {
-            this.setState({ isLoading: false, downlines: data.response })
+        .then((e) => {
+        setData(e.response)
             console.log("DATA STORED")
         })
         .catch((error) => {
-            this.setState({ requestFailed: true })
+            setrequestFailed(true)
         })
   }, [])
 
