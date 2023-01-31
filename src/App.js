cdd022331bea1d6bfa0707943a426480733652c9
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 17:57:45 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 
 
 const App = () => {
-    const [data, setData] = useState("");
+    const [data, setData] = useState({});
 
     useEffect(() => {
         const url = "https://fe-test.marketing4storage.com/cart/reserve/528560dc-0507-4db9-94f9-f1afa80d0e07";
@@ -12,7 +12,7 @@ const App = () => {
             try {
                 const response = await fetch(url);
                 const json = await response.json();
-                console.log(json.description);
+                console.log(json);
                 setData(json.description);
             } catch (error) {
                 console.log("error", error);
