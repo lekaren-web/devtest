6fac1f5a29aa84486a6404a286636846009e6e8c
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 18:22:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,38 @@ const App = () => {
     const [data, setData] = useState(null);
     const [requestFailed, setrequestFailed] = useState(null);
     const id = '528560dc-0507-4db9-94f9-f1afa80d0e07'        
-
-    const api = `https://fe-test.marketing4storage.com/cart/reserve/${id}`;
+    
+    const api = `https://fe-test.marketing4storage.com`;
+    const[ name, setName] = useState("");
+    const[ street, setStreet] = useState("");
+    const[ city, setCity] = useState("");
+    const[ state, setState] = useState("");
+    const[ zip_code, setZipCode] = useState("");
+    const[ email, setEmail] = useState("");
+    
+    const options = {
+  method: 'POST',
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  data: data,
+  url: `${api}/cart/tenant`,
+};
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    setData(e.target.value)
+    axios(options)
+    .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  };
 
    
     
      useEffect(() => {
           console.log("start call api")
-    fetch(api)
+    fetch(`${api}/cart/reserve/${id}`)
         .then((response) => {
             if(response.status === 200){
                 console.log("SUCCESSS")
