12aab83d5103e002276005f2c51b331aa8d07b14
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 17:36:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 
 function App() {
   const[test, setTes] = useState('');
   
   const fetchData =  async () => {
-   await axios.get('https://fe-test.marketing4storage.com/cart/reserve/528560dc-0507-4db9-94f9-f1afa80d0e07')
-        .then(response => {
-        console.log('heres the response:', response.data)
-        }
-        );
-  }
+  useEffect(() => {
+    const url = "https://api.adviceslip.com/advice"
+}, []);
+    
+    const fetchData = async () => {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+		} catch (error) {
+        console.log("error", error);
+		}
+};
+    
   return (
     <div className="App">
       <header className="App-header">
