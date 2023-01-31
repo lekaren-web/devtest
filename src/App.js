98f5884f7add4787c876438c527a8124c992a6f6
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 17:45:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [data, setData] = useState(null);
+	
     const fetchData = async () => {
     try {
         const response = await fetch('https://fe-test.marketing4storage.com/cart/reserve/528560dc-0507-4db9-94f9-f1afa80d0e07');
         const json = await response.json();
+	    setData(json)
         console.log(json);
 		} catch (error) {
         console.log("error", error);
@@ -18,7 +21,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>this is a test!!</code> 
+          Edit <code>this is a test!! {{data}}</code> 
         </p>
       </header>
     </div>
