393c2d95fc9acbde3ba334981e713e5154b961eb
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 18:07:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from "react";
 
 
 const App = () => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(null);
 
      useEffect(() => {
     fetch('https://fe-test.marketing4storage.com/cart/reserve/528560dc-0507-4db9-94f9-f1afa80d0e07')
       .then((res) => res.json())
       .then((resJson) => {
-        const dataCollected = JSON.parse(resJson)
+        const dataCollected = resJson
         console.log(dataCollected)
         setData(dataCollected)
     })
