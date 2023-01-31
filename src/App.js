4361e21c89b08a96ff9a9178bbe3eb65df4332c1
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 17:49:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-const [data, setData] = useState(null);
-	
-    const fetchData = async () => {
-    try {
-        const response = await fetch('https://fe-test.marketing4storage.com/cart/reserve/528560dc-0507-4db9-94f9-f1afa80d0e07');
-        const json = await response.json();
-	    setData(response.data)
-        console.log(json);
-		} catch (error) {
-        console.log("error", error);
-		}
-	};
-    
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>this is a test!! {{data}}</code> 
-        </p>
-      </header>
-    </div>
-  );
-}
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const App = () => {
+    const [advice, setAdvice] = useState("");
+
+    useEffect(() => {
+        const url = "https://api.adviceslip.com/advice";
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                console.log(json.slip.advice);
+                setAdvice(json.slip.advice);
+            } catch (error) {
+                console.log("error", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <Wrapper>
+            <Paragraph>{advice}</Paragraph>
+        </Wrapper>
+    );
+};
 
 export default App;
+
+const Wrapper = styled.div`
+    padding-top: 150px;
+    margin: 0 auto;
+`;
+
+const Paragraph = styled.h2`
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 48px;
+    text-align: center;
+`;
