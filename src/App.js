c095f4ae901556fcc836ba57ea73b4c89b78d9f9
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2023-01-31 21:01:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, 
     Route} from "react-router-dom";
-import TenantForm from "./tenantForm.js"
+import TenantForm from "./tenantForm"
 const App = () => {
     
     return (
